10e72bd9c60ed17b95e961db2d5d6a9d99461893
LastKnight12 <vovaua27@gmail.com>
2024-04-23 16:11:41 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Home from "./(navbar)/pageComponent";
-import Main from "./(main)/pageComponent";
-import Description from "./(description)/PageComponent";
-import Cards from "./(cards)/pageComponents";
+import Home from "./components/navbar/Navbar";
+import Main from "./components/main/Main";
+import Description from "./components/description/PageComponent";
+import Cards from "./components/cards/Card";
 import BGImage from "./page";
 
-const inter_800 = Inter({ subsets: ["latin"], weight: "800" });
-const inter_400 = Inter({ subsets: ["latin"]});
+const inter_400 = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Design template",
@@ -23,12 +22,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter_400.className}>
-      <BGImage />
-      <Home />
-      <Main />
-      <Description/>
-      <Cards />
-      <div style={{margin: "30px 30px 30px 0", textAlign: "end", fontSize: "1.5em", color: "#fff"}}>Site developed by Last</div>
+        <BGImage />
+        <Home />
+        <Main />
+        <Description />
+        <Cards />
+        <div
+          style={{
+            margin: "30px 30px 30px 0",
+            textAlign: "end",
+            fontSize: "1.5em",
+            color: "#fff",
+          }}
+        >
+          Site developed by Last
+        </div>
       </body>
     </html>
   );
